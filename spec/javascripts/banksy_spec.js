936d1a16c155189426a326464135a6d9f7fc3db9
jywarren <jeff@unterbahn.com>
2016-05-26 19:30:00 -0400

    entering values and triggering events in tests
    text entry using runCommand
    
    autocompletion click, event listeners, but callback hell
    
    refactored callback hell
    
    basic usage tested

--- spec/javascripts/banksy_spec.js
+++ spec/javascripts/banksy_spec.js
@@ -9,16 +9,10 @@ describe("Banksy", function() {
       parseHTML:     domador
     });
 
-  });
-
-
-  it("initializes", function() {
-
     var horse = horsey(editor.editable, {
       anchor: '@',
       suggestions: [
-        '@hodor'
-        //{ value: '@hodor',  text: 'Hodor'   },
+        { value: '@hodor',  text: 'Hodor' }
       ],
       set: function (value) {
         if (wysiwyg.mode === 'wysiwyg') {
@@ -34,7 +28,103 @@ describe("Banksy", function() {
       horse: horse
     });
 
+    enter = function(before, after) {
+
+      before = before || "";
+      after  = after  || "";
+
+      editor.runCommand(function(chunks, mode) {
+        chunks.before = chunks.before + before;
+        chunks.after = after + chunks.after;
+      });
+ 
+      // trigger the horsey:
+      crossvent.fabricate(editor.editable, "keypress");
+
+    }
+
+  });
+
+
+  it("initializes", function() {
+
     expect($('.sey-list')).not.toBe(null);
+    expect($('.sey-list .sey-item').html()).toBe("Hodor");
+
+  });
+
+
+  it("displays on @ token entry", function(done) {
+
+    editor.value('');
+    editor.setMode('wysiwyg');
+
+    enter('hello');
+    expect($('.sey-list .sey-item:first').is(':visible')).toBe(false);
+
+    enter(' @');
+
+    var onShow = function() {
+
+      expect($('.sey-list .sey-item:first').is(':visible')).toBe(true);
+      expect(editor.value()).toEqual('hello @');
+
+      crossvent.remove(editor.editable, 'horsey-show', onShow);
+      done();
+
+    }
+
+    crossvent.add(editor.editable, 'horsey-show', onShow);
+
+  });
+
+
+  it("autocompletes on selection click", function(done) {
+
+    editor.value('');
+
+    function onHide() {
+
+      expect($('.sey-list .sey-item:first').is(':visible')).toBe(false);
+
+      crossvent.add(editor.editable, 'horsey-show', onShow);
+
+      enter(' @');
+
+    }
+ 
+    function onShow() {
+
+      // Somehow, dropping priority gives time for this to run; 
+      // possibly for the element to appear and/or move?
+      setTimeout(function() {
+
+        expect($('.sey-list .sey-item:first').is(':visible')).toBe(true); // fails
+        expect(editor.value()).toEqual('hello again @');
+ 
+        crossvent.add(editor.editable, 'horsey-selected', onSelect);
+ 
+        crossvent.fabricate($('.sey-list .sey-item:first')[0], "click");
+
+      }, 0);
+
+    }
+ 
+    function onSelect() {
+ 
+      expect(editor.value()).toEqual('hello again @hodor'); // fails
+ 
+      crossvent.remove($('.sey-list .sey-item:first')[0], 'horsey-hide', onHide);
+      crossvent.remove(editor.editable, 'horsey-show', onShow);
+      crossvent.remove($('.sey-list .sey-item:first')[0], 'horsey-hide', onSelect);
+ 
+      done();
+ 
+    }
+
+    crossvent.add($('.sey-list .sey-item:first')[0], 'horsey-hide', onHide);
+
+    enter('hello again'); // clear the horsey
 
   });
 
